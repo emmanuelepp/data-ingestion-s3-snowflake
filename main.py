d711cb8895d5147b88bbf9284a8590ea6ba95a82
emmanuelepp <emmanuelponciano@gmail.com>
2025-07-29 21:42:04 -0400

    fix

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from scripts.s3_uploader import upload_file
-import os
-import sys
 from pathlib import Path
 
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 BASE_DIR = Path(__file__).resolve().parent
 data_path = BASE_DIR / "data" / "dirty_data.csv"
--- scripts/s3_uploader.py
+++ scripts/s3_uploader.py
@@ -1,28 +1,27 @@
 import boto3
 import os
-from dotenv import load_dotenv
+import logging
 from pathlib import Path
 
-# Cargar variables de entorno una vez
-load_dotenv(dotenv_path=Path(__file__).resolve(
-).parents[1] / "config" / "config.env")
+logging.basicConfig(level=logging.INFO)
 
-# Inicializar S3 una vez
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_REGION")
-)
+BASE_DIR = Path(__file__).resolve().parents[1]
 
 
-def upload_file(file_path: str):
-    bucket = os.getenv("S3_BUCKET_NAME")
-    s3_key = os.getenv("S3_FILE_KEY")
+def get_s3_client():
+    return boto3.client(
+        "s3",
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("AWS_REGION")
+    )
 
+
+def upload_file(file_path: Path, bucket: str, s3_key: str):
+    s3 = get_s3_client()
     try:
         s3.upload_file(str(file_path), bucket, s3_key)
-        print(
+        logging.info(
             f"File '{file_path}' uploaded correctly to S3 (bucket: {bucket})")
     except Exception as ex:
-        print(f"Error uploading the file to S3: {ex}")
+        logging.error(f"Error uploading the file to S3: {ex}")
