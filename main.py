3080a7bb47bb9c23be819a95d0777bf1be8ff785
emmanuelepp <emmanuelponciano@gmail.com>
2025-08-10 20:34:04 -0400

    fix

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from scripts.s3_uploader import upload_file
+from scripts.snowflake_client import get_snowflake_conn
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
 
 BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(BASE_DIR / "config" / "config.env")
+
 data_path = BASE_DIR / "data" / "dirty_data.csv"
 
 if __name__ == '__main__':
-    upload_file(data_path)
+    upload_file(
+        data_path,
+        bucket=os.getenv("AWS_BUCKET_NAME"),
+        s3_key=os.getenv("AWS_S3_KEY")
+    )
+    get_snowflake_conn()
--- scripts/snowflake_client.py
+++ scripts/snowflake_client.py
@@ -1,1 +1,24 @@
-# import snowflake.connector
+import snowflake.connector
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv(Path(__file__).resolve().parents[1] / "config" / "config.env")
+
+
+def get_snowflake_conn():
+    try:
+        conn = snowflake.connector.connect(
+            account=os.getenv("SNOWFLAKE_ACCOUNT"),
+            user=os.getenv("SNOWFLAKE_USER"),
+            password=os.getenv("SNOWFLAKE_PASSWORD"),
+            role=os.getenv("SNOWFLAKE_ROLE"),
+            warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+            database=os.getenv("SNOWFLAKE_DATABASE"),
+            schema=os.getenv("SNOWFLAKE_SCHEMA"),
+        )
+        print("Connected to Snowflake!")
+        return conn
+    except Exception as e:
+        print(f"Failed to connect to Snowflake: {e}")
+        return None
